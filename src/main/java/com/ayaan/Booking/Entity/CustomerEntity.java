bad884d04e8d0a76f03b3b5feee90720b50acb45
Ayaan Pathan <ayaanpathanra@gmail.com>
2026-08-05 22:27:37 +0530

    add customer entity

---
+++ src/main/java/com/ayaan/Booking/Entity/CustomerEntity.java
@@ -0,0 +1,40 @@
+package com.ayaan.Booking.Entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+@Entity
+@Table(
+        name = "customers",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uq_customers_phone",
+                        columnNames = "phone"
+                )
+        }
+)
+public class CustomerEntity extends com.ayaan.booking.entity.BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            nullable = false,
+            length = 100
+    )
+    private String name;
+
+    @Column(
+            nullable = false,
+            length = 20
+    )
+    private String phone;
+}
